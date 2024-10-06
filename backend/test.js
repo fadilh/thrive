ffee1d86c1ed04124c1c0d749f30a067a3e2d7e6
hmemon06 <hmemon@utexas.edu>
2024-10-06 00:03:45 -0500

    finished w crud for mongo

--- backend/test.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();  // Load .env file
-
-const express = require('express');
-console.log('ES Module test');
-
-const uri = process.env.ATLAS_URI;
-console.log('ATLAS_URI:', uri);
